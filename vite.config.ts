857f115d929307e07dd2583d236c8d99ea529c80
Mohammad Mahdi Shahbazi <162153592+ByShahbazi@users.noreply.github.com>
2025-11-26 15:05:45 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: "/Portfolio/",   // برای GitHub Pages ضروری است
+    base: "/Portfolio/", 
 
     server: {
       port: 3000,
